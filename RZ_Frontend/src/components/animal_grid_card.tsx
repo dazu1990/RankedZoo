c0b3fa7fbe8a31b4d6179d0b266f81e107e14eba
Adrian Davila-Zuniga <adriandz@Adrians-MacBook-Air.local>
2024-10-23 20:44:04 -0400

    added newspaper icon

--- RZ_Frontend/src/components/animal_grid_card.tsx
+++ RZ_Frontend/src/components/animal_grid_card.tsx
@@ -1,14 +1,9 @@
 import React , {useCallback, useEffect, useState} from 'react';
-import { LockClosedIcon } from '@heroicons/react/24/solid';
+import { LockClosedIcon, NewspaperIcon } from '@heroicons/react/24/solid';
+
 import { Draggable } from 'react-beautiful-dnd';
 import { RANK_VALUES } from '../constants';
 
-
-
-import { useFetchAnimal } from '../hooks/useFetchAnimal';
-
-import { useJwtAuth } from '../hooks/useJwtAuth';
-import { useUpdateAnimalRank } from '../hooks/useUpdateRank';
 import { roundToTwo } from '../util';
 
 
@@ -31,20 +26,22 @@ interface AnimalGridCardProps {
     letterRank: string,
 }
 
-const class_CardTopLayer = (image : string | undefined) =>`
-    z-0 
-    absolute 
-    top-0 
-    w-full 
-    transition-all 
-    ${image ? `opacity-0` : `opacity-100`} 
-    hover:opacity-100
-    flex
-    justify-center
-    items-center
-    align-middle
-    h-full
-`
+const class_CardTopLayer = (image : string | undefined, locked: boolean) =>`
+        z-0 
+        absolute 
+        top-0 
+        w-full 
+        transition-all 
+        ${image ? `opacity-0` : 'opacity-100'} 
+        flex
+        justify-center
+        items-center
+        align-middle
+        h-full
+        hover: opacity-100
+        // hover: ${locked ? `opacity-100` : 'opacity-100'} 
+
+    `
 
 const class_CardTopLayerTextWrapper = (letterRankIndex : number) => `
     font-bold 
@@ -69,39 +66,11 @@ const class_CardTopLayerTextWrapper = (letterRankIndex : number) => `
 
 export const AnimalGridCard = ( {...props} : AnimalGridCardProps) => {
 
-    const [sendUpdate] = useUpdateAnimalRank();
 
-    const token = useJwtAuth();
-    const [rank, setRank] = useState<number>(props.currentRawRank);
-    const [overallRank,setOverallRank] = useState<number>(props.currentRawRank);
-    const [updateRankIncrement, setUpdateRankIncrement] = useState<number>(1);
-    const [locked, setLocked] = useState<boolean>(props.locked);
-    const [newRankCount, setNewRankCount] = useState<number>(0);
+    const [rank] = useState<number>(props.currentRawRank);
+    const [locked] = useState<boolean>(props.locked);
 
     console.log('locked', locked, props.name);
-
-   
-
-
-    const handleUpdateRank = (increment: number) => {
-
-        sendUpdate(props.id, props.name, increment, token);
-
-        // props.getNewRank(props.id);
-
-        //remove
-        setNewRankCount(newRankCount + 1);
-        //remove
-        const newRank = roundToTwo((props.rankingSum + increment) / (props.rankingsCount + newRankCount));
-        //remove
-        setOverallRank(newRank)
-
-        setRank(newRank);
-        setLocked(true);
-        setTimeout(() => {
-            setLocked(false);
-        }, 1000);
-    };
 
 
     return (
@@ -120,28 +89,38 @@ export const AnimalGridCard = ( {...props} : AnimalGridCardProps) => {
                 >
                     <div className="w-full overflow-hidden  relative">
                        
-                        {locked && (
+                        {/* {locked && (
                             <div className="top-1 right-1 z-0 h-5 w-5 absolute">
                                 <LockClosedIcon className="size-6 text-black" />
                             </div>
-                        )}
+                        )} */}
                         {props.image && (
-                            <div className="w-full  z-1 relative  mix-blend-multiply">
-                                <img className="h-gridRow object-cover" src={props.image} alt={props.name} />
-
+                            <div className={`w-full  z-1 relative  mix-blend-multiply `}>
+                                {/* ${locked ? 'opacity-80' : 'opacity-100'} */}
+                                <img className={`h-gridRow object-cover `} src={props.image} alt={props.name} />
                             </div>
                         )}
                         
                         {!props.image && (
-                        <div className="w-full h-gridRow z-1 relative"></div>
+                        <div className="w-full h-gridRow z-1 relative flex justify-center">image missing</div>
                         )}
                         
 
-                        <div className={class_CardTopLayer(props.image)}>
+                        <div className={class_CardTopLayer(props.image, locked)}>
                             <div className={class_CardTopLayerTextWrapper(props.letterRankIndex)}>
-                                {props.name}
-                                <br></br>
-                                {locked ? (<LockClosedIcon className="h-10 w-10 text-black" />) : roundToTwo(rank)}
+                                {/* {!locked && ( 
+                                    <>
+                                        {props.name} 
+                                        {roundToTwo(rank)}
+                                    </>
+                                ) } */}
+                                {props.name} 
+                                        {roundToTwo(rank)}
+                                {locked && (<LockClosedIcon className="h-10 w-10 text-black" />) }
+                                <div onClick={()=>alert('hi')} className="h-5 w-5 text-black cursor-pointer hover:text-white">
+                                    <NewspaperIcon></NewspaperIcon>
+                                </div>
+                                
                             </div>
                     
                             {/* <div className='flex w-full justify-between'>
@@ -189,7 +168,5 @@ export const AnimalGridCard = ( {...props} : AnimalGridCardProps) => {
                 </div>
             )}
         </Draggable>
-
-        
     )
 };
